d7008d53578a10cc60790621f22ff8389668f469
Karthik Bhat A <karthik.bhata7@gmail.com>
2017-03-20 00:21:35 +0530

    printing the payload, the printing function for u_char is taken from sniffex.c

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <netinet/if_ether.h>
 #include "packet_structures.h"
 
-#define endstream endl<<"-> ";
+#define endstream endl<<"-> "
 #define IFACE_NAME 100
 
 using namespace std;
@@ -21,6 +21,8 @@ void my_packet_handler(
 void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);
 void payload_analyze(const u_char *packet, struct pcap_pkthdr packet_header);
 void endpacket();
+void print_payload(const u_char *, int );
+void print_hex_ascii_line(const u_char *, int, int);
 
 
 int main(int argc, char const *argv[]) {
@@ -38,6 +40,8 @@ int main(int argc, char const *argv[]) {
 	int promisc = 1;
 	int timeout_limit = 10000;
 	int iface_sel, i, lookup_return_code;
+  struct bpf_program filter;
+  char filter_exp[] = "";
 
 	if(pcap_findalldevs(&interfaces, errbuf)==-1) {
 		cerr<<"Couldn't recognize network interfaces"<<endl;
@@ -89,15 +93,36 @@ int main(int argc, char const *argv[]) {
 	cout<<"Subnet mask: "<<subnet_mask<<endl;
 	cout<<endl;
 
-	handle = pcap_open_live(
-		dev,
-		BUFSIZ,
-		promisc,
-		timeout_limit,
-		errbuf
-	);
+	handle = pcap_create(dev, errbuf);
+
+  if (pcap_can_set_rfmon(handle) == 0) {
+    cerr<<"Cannot set monitor mode. "<<pcap_geterr(handle)<<endl;
+    exit(-1);
+  }
+
+  pcap_set_promisc(handle, 1);
+  pcap_set_snaplen(handle, 2048);
+  pcap_set_timeout(handle, 1000);
+
+  if (pcap_activate(handle) !=0 ) {
+    cerr<<"Error activating capture handle. "<<pcap_geterr(handle)<<endl;
+    exit(-1);
+  }
+
+
+  if(pcap_compile(handle, &filter, filter_exp, 0, subnet_mask_raw)==-1) {
+    cerr<<"Bad filter - "<<pcap_geterr(handle)<<endl;
+    exit(-1);
+  }
+
+  if(pcap_setfilter(handle, &filter) == -1) {
+    cerr<<"Error setting the filter - "<<pcap_geterr(handle)<<endl;
+    exit(-1);
+  }
 
 	pcap_loop(handle, 0, my_packet_handler, NULL);
+
+  pcap_close(handle);
 
   return 0;
 
@@ -199,11 +224,112 @@ void payload_analyze(const u_char *packet, struct pcap_pkthdr packet_header) {
 
   payload = packet + total_header_len; // Payload starting location
   printf("Memory address where payload begins: %p\n\n", payload);
+  print_payload(payload, payload_len);
 
   endpacket();
   return;
 
 }
+
+/*
+ * print packet payload data (avoid printing binary data)
+ */
+void
+print_payload(const u_char *payload, int len)
+{
+
+	int len_rem = len;
+	int line_width = 16;			/* number of bytes per line */
+	int line_len;
+	int offset = 0;					/* zero-based offset counter */
+	const u_char *ch = payload;
+
+	if (len <= 0)
+		return;
+
+	/* data fits on one line */
+	if (len <= line_width) {
+		print_hex_ascii_line(ch, len, offset);
+		return;
+	}
+
+	/* data spans multiple lines */
+	for ( ;; ) {
+		/* compute current line length */
+		line_len = line_width % len_rem;
+		/* print line */
+		print_hex_ascii_line(ch, line_len, offset);
+		/* compute total remaining */
+		len_rem = len_rem - line_len;
+		/* shift pointer to remaining bytes to print */
+		ch = ch + line_len;
+		/* add offset */
+		offset = offset + line_width;
+		/* check if we have line width chars or less */
+		if (len_rem <= line_width) {
+			/* print last line and get out */
+			print_hex_ascii_line(ch, len_rem, offset);
+			break;
+		}
+	}
+
+return;
+}
+
+/*
+ * print data in rows of 16 bytes: offset   hex   ascii
+ *
+ * 00000   47 45 54 20 2f 20 48 54  54 50 2f 31 2e 31 0d 0a   GET / HTTP/1.1..
+ */
+void
+print_hex_ascii_line(const u_char *payload, int len, int offset)
+{
+
+	int i;
+	int gap;
+	const u_char *ch;
+
+	/* offset */
+	printf("%05d   ", offset);
+
+	/* hex */
+	ch = payload;
+	for(i = 0; i < len; i++) {
+		printf("%02x ", *ch);
+		ch++;
+		/* print extra space after 8th byte for visual aid */
+		if (i == 7)
+			printf(" ");
+	}
+	/* print space to handle line less than 8 bytes */
+	if (len < 8)
+		printf(" ");
+
+	/* fill hex gap with spaces if not full line */
+	if (len < 16) {
+		gap = 16 - len;
+		for (i = 0; i < gap; i++) {
+			printf("   ");
+		}
+	}
+	printf("   ");
+
+	/* ascii (if printable) */
+	ch = payload;
+	for(i = 0; i < len; i++) {
+		if (isprint(*ch))
+			printf("%c", *ch);
+		else
+			printf(".");
+		ch++;
+	}
+
+	printf("\n");
+
+return;
+}
+
+
 
 
 void endpacket() {
