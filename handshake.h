b2f7da7d2dd5aa630030ad8b80fff69afe623837
Karthik Bhat A <karthik.bhata7@gmail.com>
2017-06-17 23:01:46 +0530

    server hello, cert, server_hello_done

--- handshake.h
+++ handshake.h
@@ -13,7 +13,38 @@ void certificate_verify(const u_char *);
 void client_key_exchange(const u_char *);
 void finished(const u_char *);
 void print_hex(const u_char *, int);
+void print_ciphersuites(const u_char *, int );
+void print_certificates(const u_char *, int);
 
+void print_certificates(const u_char *certs, int len) {
+  int parsed = 0;
+  int a, b, c, certificate_length;
+  while(len-parsed) {
+    a = (int)*(certs); // second
+    b = (int)*(certs+1); // third
+    c = (int)*(certs+2); // furth byte
+    certificate_length = (a<<16) + (b<<8) + (c);
+    cout<<"certificate length: "<<certificate_length<<endl;
+    certs = certs + 3;
+    print_payload(certs, certificate_length);
+    certs = certs + certificate_length;
+    parsed = 3 + certificate_length;
+  }
+  return;
+}
+
+
+void print_ciphersuites(const u_char *data, int len) {
+  int parsed = 0;
+  short int suite;
+  while(len-parsed) {
+    suite = *(short int*)data;
+    cout<< hex << suite << dec << endl;
+    data = data + 2;
+    parsed = parsed + 2;
+  }
+  return;
+}
 
 void print_hex(const u_char *data, int len) {
   int val;
@@ -125,7 +156,8 @@ void client_hello(const u_char *body)
   cout<<"cipher suite length: "<<ciphersuite_len<<endl;
   body = body + 2;
 
-  cout<<"follows.. "<<(ciphersuite_len/2)<<" suites"<<endl;
+  cout<<"cipher suites: "<<endl;
+  print_ciphersuites(body, ciphersuite_len);
   body = body + ciphersuite_len;
 
   int compression_len = (int)*body;
@@ -149,11 +181,82 @@ void client_hello(const u_char *body)
 
 void server_hello(const u_char *body)
 {
+  int a = (int)*(body+1); // second
+  int b = (int)*(body+2); // third
+  int c = (int)*(body+3); // furth byte
+  int length = (a<<16) + (b<<8) + (c);
+  cout<<"client hello length: "<<length<<endl;
+  body = body + 4;
+  short int version = *(short int *)(body);
+  cout<<"Version: ";
+  switch (version) {
+    case 0x0300:
+    cout<<"SSL 3.0"<<endl;
+    break;
+    case 0x0301:
+    cout<<"TLS 1.1"<<endl;
+    break;
+    case 0x0302:
+    cout<<"TLS 1.2"<<endl;
+    break;
+    case 0x0303:
+    cout<<"TLS 1.3"<<endl;
+    break;
+    default:
+    cout<<"failed to decode"<<endl;
+    break;
+  }
+  body = body + 2;
+
+  cout<<"Random: ";
+  print_hex(body, 32);
+  body = body + 32;
+
+  int sid_len = (int)*body;
+  cout<<"session id length: "<<(int)sid_len<<endl;
+  body = body + 1;
+
+  cout<<"session id: ";
+  print_hex(body, sid_len);
+  body = body + sid_len;
+
+  short int cipher_suite = *(short int *)(body);
+  cout<< "cipher suite: " << hex << cipher_suite << dec << endl;
+  body = body + 2;
+
+  int compression_method = (int)*body;
+  cout<<"compression method: "<<compression_method<<"(null)"<<endl;
+  body = body + 1;
+
+  short int extenstions_len = *(short int*)body;
+  extenstions_len = (extenstions_len>>8) | (extenstions_len<<8);
+  cout<<"extenstions length: "<<extenstions_len<<endl;
+  body = body + 2;
+
+  cout<<"follows.. extenstions"<<endl;
+  body = body + extenstions_len;
   return;
 }
 
 void certificate(const u_char *body)
 {
+  int a = (int)*(body+1); // second
+  int b = (int)*(body+2); // third
+  int c = (int)*(body+3); // furth byte
+  int length = (a<<16) + (b<<8) + (c);
+  cout<<"length: "<<length<<endl;
+  body = body + 4;
+
+  a = (int)*(body);   // second
+  b = (int)*(body+1); // third
+  c = (int)*(body+2); // furth byte
+  int certificates_length = (a<<16) + (b<<8) + (c);
+  cout<<"certificates length: "<<certificates_length<<endl;
+  body = body + 3;
+
+  print_certificates(body, certificates_length);
+  body = body + certificates_length;
+
   return;
 }
 
@@ -169,6 +272,12 @@ void certificate_request(const u_char *body)
 
 void server_hello_done(const u_char *body)
 {
+  int a = (int)*(body+1); // second
+  int b = (int)*(body+2); // third
+  int c = (int)*(body+3); // furth byte
+  int length = (a<<16) + (b<<8) + (c);
+  cout<<"length: "<<length<<endl;
+  body = body + 4;
   return;
 }
 
